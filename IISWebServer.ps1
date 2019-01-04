1803bda12a439ba89e8157185abc2c69ab3b5196
Mukesh Fulewale <mukeshfulewale@gmail.com>
2019-01-04 15:16:34 +0530

    Add files via upload

---
+++ IISWebServer.ps1
@@ -0,0 +1,24 @@
+﻿configuration IISWebServer
+{   
+    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'  
+  
+    Node "TestIIS"  
+    {   
+        WindowsFeature IIS   
+        {   
+            Name = “Web-Server”                          
+            Ensure = “Present”   
+        }   
+        WindowsFeature InstallAspDotNet45  
+        {  
+            Name = "Web-Asp-Net45"  
+            Ensure = "Present"  
+        }  
+        WindowsFeature NET3.5  
+        {  
+            Name= "NET-Framework-Features"  
+            Ensure = "Present"  
+        }  
+    }   
+}  
+IISWebServer
